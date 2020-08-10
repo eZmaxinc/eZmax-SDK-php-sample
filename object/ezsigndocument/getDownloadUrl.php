43332f61c4952c8800ffb3bd888c84b59cbb7eac
gallatoe <69307493+gallatoe@users.noreply.github.com>
2020-08-10 11:54:00 -0400

    Develop (#3)
    * add getDownloadUrl
    
    * qqqq
    
    * Add exemple to get URL to signed in person and add exemple to download signed document and proof of signed document.
    
    * Supression header 204. Changement fonction de download pour les fichiers.

--- object/ezsigndocument/getDownloadUrl.php
+++ object/ezsigndocument/getDownloadUrl.php
@@ -1,2 +1,90 @@
 <?php
+
+/**
+ * This sample shows how to get proof or sign dopcument for one document signed.
+* In this example, we put the pki and document type in constant. We will get three files :
+* 1) signed ezsign document by setting the pki in a constant
+* 2) complete proof in an zip
+* 2) proof document in pdf
+*
+*/
+
+//Specifying namespaces we are using below to make the creation of objects easier to read.
+use eZmaxAPI\Api\EzsigndocumentApi;
+use eZmaxAPI\Model\EzsigndocumentGetDownloadUrlV1Response;
+use eZmaxAPI\Model\EzsigndocumentRequest;
+
+
+/*
+ * The pkiEzsigndocumentID we wish to get.
+ * This value was returned after a successful ezsigndocumentCreateObject.
+ */
+
+define ('SAMPLE_pkiEzsigndocumentID', 243);
+
+/*
+ * The type of document we wish to get.
+ * This values was defined in documentation.
+ */
+
+define ('SAMPLE_sDocumentTypeSigned', 'Signed');
+define ('SAMPLE_sDocumentTypeProof', 'Proof');
+define ('SAMPLE_sDocumentTypeProofdocument', 'Proofdocument');
+
+require_once (__DIR__ . '/../../connector.php');
+
+$objEzsigndocumentApi = new EzsigndocumentApi(new GuzzleHttp\Client(), $objConfiguration);
+
+try {
+
+	/*
+	 * We need to pass the pkiID of the ezsign document we want to get and the type of document we want : "Signed" for signed document.
+	 * We got it from a call to a previous ezsigndocumentCreateObject
+	 */
+	$objEzsigndocumentGetDownloadUrlV1Response = $objEzsigndocumentApi->ezsigndocumentGetDownloadUrlV1(SAMPLE_pkiEzsigndocumentID,SAMPLE_sDocumentTypeSigned);
+
+	// Let's retrieve some data from the object and display it
+	//echo 'Url: '.$objEzsigndocumentGetDownloadUrlV1Response->getMPayload()->getSDownloadUrl().PHP_EOL;
+
+	// Download and save file to disk. File is transmit in gzip format. We use wrapper "compress.zlib://" to uncompress it.
+	file_put_contents(__DIR__ . '/document.pdf', file_get_contents('compress.zlib://'.$objEzsigndocumentGetDownloadUrlV1Response->getMPayload()->getSDownloadUrl()));
+
+
+	/*
+	 * We need to pass the pkiID of the ezsign document we want to get and the type of document we want : "Proof" for the complete proof.
+	 * We got it from a call to a previous ezsigndocumentCreateObject
+	 */
+	$objEzsigndocumentGetDownloadUrlV1Response = $objEzsigndocumentApi->ezsigndocumentGetDownloadUrlV1(SAMPLE_pkiEzsigndocumentID,SAMPLE_sDocumentTypeProof);
+
+	// Let's retrieve some data from the object and display it
+	//echo 'Url: '.$objEzsigndocumentGetDownloadUrlV1Response->getMPayload()->getSDownloadUrl().PHP_EOL;
+
+	//Uncomment this line to ouput complete response
+	//print_r($objEzsigndocumentGetDownloadUrlV1Responsee);
+
+	// Download and save file to disk. File is transmit in gzip format. We use wrapper "compress.zlib://" to uncompress it.
+	file_put_contents(__DIR__ . '/proof.zip', file_get_contents('compress.zlib://'.$objEzsigndocumentGetDownloadUrlV1Response->getMPayload()->getSDownloadUrl()));
+
+
+	/*
+	 * We need to pass the pkiID of the ezsign document we want to get and the type of document we want : "Proof" for the proof pdf document.
+	 * We got it from a call to a previous ezsigndocumentCreateObject
+	 */
+	$objEzsigndocumentGetDownloadUrlV1Response = $objEzsigndocumentApi->ezsigndocumentGetDownloadUrlV1(SAMPLE_pkiEzsigndocumentID,SAMPLE_sDocumentTypeProofdocument);
+
+	// Let's retrieve some data from the object and display it
+	//echo 'Url: '.$objEzsigndocumentGetDownloadUrlV1Response->getMPayload()->getSDownloadUrl().PHP_EOL;
+
+	// Download and save file to disk. File is transmit in gzip format. We use wrapper "compress.zlib://" to uncompress it.
+	file_put_contents(__DIR__ . '/proofdocument.pdf', file_get_contents('compress.zlib://'.$objEzsigndocumentGetDownloadUrlV1Response->getMPayload()->getSDownloadUrl()));
+
+
+	echo 'File downloaded to '.__DIR__ . '/document.pdf'.PHP_EOL;
+	echo 'Proof archive downloaded to '.__DIR__ . '/proof.zip'.PHP_EOL;
+	echo 'Proof document downloaded to '.__DIR__ . '/proofdocument.pdf'.PHP_EOL;
+}
+catch (Exception $e) {
+	print_r($e);
+}
+
 ?>
---
+++ object/ezsignfoldersignerassociation/getInPersonLoginUrl.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This sample shows how to get one Login URL to sign in person.
+* In this example, we will get an URL by setting the pki in a constant
+*
+*/
+
+//Specifying namespaces we are using below to make the creation of objects easier to read.
+use eZmaxAPI\Api\EzsignfoldersignerassociationApi;
+
+/*
+ * The pkiEzsigndocumentID we wish to get.
+ * This value was returned after a successful ezsigndocumentCreateObject.
+ */
+
+define ('SAMPLE_pkiEzsignfoldersignerassociationID', 112);
+
+require_once (__DIR__ . '/../../connector.php');
+
+$objEzsignfoldersignerassociationApi = new EzsignfoldersignerassociationApi(new GuzzleHttp\Client(), $objConfiguration);
+
+try {
+
+	/*
+	 * We need to pass the pkiID of the ezsign document we want to get and the type of document we want : "Signed" for signed document.
+	 * We got it from a call to a previous ezsigndocumentCreateObject
+	 */
+	$objEzsigndocumentGetLoginUrlV1Response = $objEzsignfoldersignerassociationApi->ezsignfoldersignerassociationGetInPersonLoginUrlV1(SAMPLE_pkiEzsignfoldersignerassociationID);
+
+	// Let's retrieve some data from the object and display it
+	echo 'Url: '.$objEzsigndocumentGetLoginUrlV1Response->getMPayload()->getSLoginUrl().PHP_EOL;
+
+	//Uncomment this line to ouput complete response
+	//print_r($objEzsigndocumentGetLoginUrlV1Response);
+
+}
+catch (Exception $e) {
+	print_r($e);
+}
+
+?>
